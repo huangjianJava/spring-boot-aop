26140376343a191bd1b781a61237e388d24b1c7e
huangjian <huangjian@sinter.cn>
2018-08-29 18:30:12 +0800

    code review

--- src/main/java/com/henry/utils/SinterUtil.java
+++ src/main/java/com/henry/utils/SinterUtil.java
@@ -9,7 +9,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.security.MessageDigest;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -34,26 +33,6 @@ public class SinterUtil {
      * @return
      * @throws Exception
      */
-    public static Map<String, String> assembleSinterParams(String requestData) throws Exception {
-        String dataSign = SinterUtil.encrypt(requestData, API_KEY, Charsets.UTF_8.name());
-        String currentTimeMillis = String.valueOf(System.currentTimeMillis());
-
-        Map<String, String> params = Maps.newHashMap();
-        params.put("businessID", BUSINESS_ID);
-        params.put("dataType", "json");
-        params.put("timestamp", currentTimeMillis);
-        params.put("requestData", urlEncoder(requestData, Charsets.UTF_8.name()));
-        params.put("dataSign", urlEncoder(dataSign, Charsets.UTF_8.name()));
-        return params;
-    }
-
-    /**
-     * 组装调用森特尔接口的参数
-     *
-     * @param requestData
-     * @return
-     * @throws Exception
-     */
     public static SinterInterfaceReqDto assembleSinterParamsTwo(String requestData) throws Exception {
         String dataSign = SinterUtil.encrypt(requestData, API_KEY, Charsets.UTF_8.name());
         String currentTimeMillis = String.valueOf(System.currentTimeMillis());
@@ -64,78 +43,6 @@ public class SinterUtil {
                 .requestData(requestData)
                 .dataSign(dataSign)
                 .build();
-    }
-
-    /**
-     * 向指定 URL 发送POST方法的请求
-     *
-     * @param url    发送请求的 URL
-     * @param params 请求的参数集合
-     * @return 远程资源的响应结果
-     */
-    public static String sendPost(String url, Map<String, String> params) {
-        OutputStreamWriter out = null;
-        BufferedReader in = null;
-        StringBuilder result = new StringBuilder();
-        try {
-            URL realUrl = new URL(url);
-            HttpURLConnection conn = (HttpURLConnection) realUrl.openConnection();
-            // 发送POST请求必须设置如下两行
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            // POST方法
-            conn.setRequestMethod("POST");
-            // 设置通用的请求属性
-            conn.setRequestProperty("accept", "*/*");
-            conn.setRequestProperty("connection", "Keep-Alive");
-            conn.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            //conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.connect();
-            // 获取URLConnection对象对应的输出流
-            out = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
-            // 发送请求参数
-            if (params != null) {
-                StringBuilder param = new StringBuilder();
-                for (Map.Entry<String, String> entry : params.entrySet()) {
-                    if (param.length() > 0) {
-                        param.append("&");
-                    }
-                    param.append(entry.getKey());
-                    param.append("=");
-                    param.append(entry.getValue());
-                    System.out.println(entry.getKey() + ":" + entry.getValue());
-                }
-                System.out.println("param:" + param.toString());
-                out.write(param.toString());
-            }
-            // flush输出流的缓冲
-            out.flush();
-            // 定义BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream(), "UTF-8"));
-            String line;
-            while ((line = in.readLine()) != null) {
-                result.append(line);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        //使用finally块来关闭输出流、输入流
-        finally {
-            try {
-                if (out != null) {
-                    out.close();
-                }
-                if (in != null) {
-                    in.close();
-                }
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-        return result.toString();
     }
 
     /**
